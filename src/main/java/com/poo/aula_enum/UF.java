0ad037a14a2e838fb67e64963294e98c4e7f997c
Yuri Moreira Leocadio <137840146+YuriLeocadio@users.noreply.github.com>
2024-07-16 23:48:10 +0000

    Modificação do Enum UF para incluir Regiões

--- src/main/java/com/poo/aula_enum/UF.java
+++ src/main/java/com/poo/aula_enum/UF.java
@@ -2,42 +2,48 @@ package com.poo.aula_enum;
 
 public enum UF {
 
-    AC("Acre"),
-    AL("Alagoas"),
-    AP("Amapá"),
-    AM("Amazonas"),
-    BA("Bahia"),
-    CE("Ceará"),
-    DF("Distrito Federal"),
-    ES("Espírito Santo"),
-    GO("Goiás"),
-    MA("Maranhão"),
-    MT("Mato Grosso"),
-    MS("Mato Grosso do Sul"),
-    MG("Minas Gerais"),
-    PA("Pará"),
-    PB("Paraíba"),
-    PR("Paraná"),
-    PE("Pernambuco"),
-    PI("Piauí"),
-    RJ("Rio de Janeiro"),
-    RN("Rio Grande do Norte"),
-    RS("Rio Grande do Sul"),
-    RO("Rondônia"),
-    RR("Roraima"),
-    SC("Santa Catarina"),
-    SP("São Paulo"),
-    SE("Sergipe"),
-    TO("Tocantins");
+    AC("Acre", Regiao.NORTE),
+    AL("Alagoas", Regiao.NORDESTE),
+    AP("Amapá", Regiao.NORTE),
+    AM("Amazonas", Regiao.NORTE),
+    BA("Bahia", Regiao.NORDESTE),
+    CE("Ceará", Regiao.NORDESTE),
+    DF("Distrito Federal", Regiao.CENTRO_OESTE),
+    ES("Espírito Santo", Regiao.SUDESTE),
+    GO("Goiás", Regiao.CENTRO_OESTE),
+    MA("Maranhão", Regiao.NORDESTE),
+    MT("Mato Grosso", Regiao.CENTRO_OESTE),
+    MS("Mato Grosso do Sul", Regiao.CENTRO_OESTE),
+    MG("Minas Gerais", Regiao.SUDESTE),
+    PA("Pará", Regiao.NORTE),
+    PB("Paraíba", Regiao.NORDESTE),
+    PR("Paraná", Regiao.SUL),
+    PE("Pernambuco", Regiao.NORDESTE),
+    PI("Piauí", Regiao.NORDESTE),
+    RJ("Rio de Janeiro", Regiao.SUDESTE),
+    RN("Rio Grande do Norte", Regiao.NORDESTE),
+    RS("Rio Grande do Sul", Regiao.SUL),
+    RO("Rondônia", Regiao.NORTE),
+    RR("Roraima", Regiao.NORTE),
+    SC("Santa Catarina", Regiao.SUL),
+    SP("São Paulo", Regiao.SUDESTE),
+    SE("Sergipe", Regiao.NORDESTE),
+    TO("Tocantins", Regiao.NORTE);
 
     private final String nomePorExtenso;
+    private final Regiao regiao;
 
-    UF(String nomePorExtenso) {
+    UF(String nomePorExtenso, Regiao regiao) {
         this.nomePorExtenso = nomePorExtenso;
+        this.regiao = regiao;
     }
 
     public String getNomePorExtenso() {
         return nomePorExtenso;
+    }
+
+    public Regiao getRegiao() {
+        return regiao;
     }
 
     public static boolean validarSigla(String sigla) {
