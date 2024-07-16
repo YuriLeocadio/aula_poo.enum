85b04644501af84522eedc08dae7ce9dbc56f300
Yuri Moreira Leocadio <yurileocadio0411@gmail.com>
2024-07-16 19:39:27 -0300

    first commit

---
+++ src/main/java/com/poo/aula_enum/AulaEnumApplication.java
@@ -0,0 +1,13 @@
+package com.poo.aula_enum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaEnumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaEnumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poo/aula_enum/AulaEnumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poo.aula_enum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaEnumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
