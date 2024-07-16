152cb5132793e9bd0d8c1771c102d8a36e1f29d1
Yuri Moreira Leocadio <137840146+YuriLeocadio@users.noreply.github.com>
2024-07-16 23:15:28 +0000

    Criação do Enum UF

---
+++ src/main/java/com/poo/aula_enum/UF.java
@@ -0,0 +1,42 @@
+package com.poo.aula_enum;
+
+public enum UF {
+    
+    AC("Acre"),
+    AL("Alagoas"),
+    AP("Amapá"),
+    AM("Amazonas"),
+    BA("Bahia"),
+    CE("Ceará"),
+    DF("Distrito Federal"),
+    ES("Espírito Santo"),
+    GO("Goiás"),
+    MA("Maranhão"),
+    MT("Mato Grosso"),
+    MS("Mato Grosso do Sul"),
+    MG("Minas Gerais"),
+    PA("Pará"),
+    PB("Paraíba"),
+    PR("Paraná"),
+    PE("Pernambuco"),
+    PI("Piauí"),
+    RJ("Rio de Janeiro"),
+    RN("Rio Grande do Norte"),
+    RS("Rio Grande do Sul"),
+    RO("Rondônia"),
+    RR("Roraima"),
+    SC("Santa Catarina"),
+    SP("São Paulo"),
+    SE("Sergipe"),
+    TO("Tocantins");
+
+    private final String nomePorExtenso;
+
+    UF(String nomePorExtenso) {
+        this.nomePorExtenso = nomePorExtenso;
+    }
+
+    public String getNomePorExtenso() {
+        return nomePorExtenso;
+    }
+}
