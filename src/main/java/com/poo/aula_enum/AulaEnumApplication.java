c465bfcfdee1cc2dd08cb482b4f6bac802b2d8ad
Yuri Moreira Leocadio <137840146+YuriLeocadio@users.noreply.github.com>
2024-07-17 00:06:55 +0000

    Criação de um menu para escolher a atividade desejada e criação de novos métodos

--- src/main/java/com/poo/aula_enum/AulaEnumApplication.java
+++ src/main/java/com/poo/aula_enum/AulaEnumApplication.java
@@ -13,7 +13,29 @@ public class AulaEnumApplication {
 		limpa();
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Escolha uma UF (sigla): ");
+		System.out.println("Escolha uma atividade:");
+		System.out.println("1. Escolher uma UF");
+		System.out.println("2. Escolher uma Região");
+		System.out.print("Opção: ");
+		int opcao = sc.nextInt();
+		sc.nextLine();
+
+		switch (opcao) {
+			case 1:
+				escolherUF(sc);
+				break;
+			case 2:
+				escolherRegiao(sc);
+				break;
+			default:
+				System.out.println("Opção inválida.");
+		}
+
+		sc.close();
+	}
+
+	private static void escolherUF(Scanner sc) {
+		System.out.println("Escolha uma UF (sigla):");
 		for (UF uf : UF.values()) {
 			System.out.print(uf.name() + " ");
 		}
@@ -22,27 +44,53 @@ public class AulaEnumApplication {
 		String ufEscolhida = sc.nextLine().toUpperCase();
 
 		if (UF.validarSigla(ufEscolhida)) {
-            UF uf = UF.valueOf(ufEscolhida);
-            System.out.println("A UF escolhida foi: " + uf.getNomePorExtenso());
-        } else {
-            System.out.println("Sigla de UF inválida.");
-        }
+			UF uf = UF.valueOf(ufEscolhida);
+			System.out.println("Você escolheu: " + uf.getNomePorExtenso());
+		} else {
+			System.out.println("Sigla de UF inválida.");
+		}
+	}
 
-		sc.close();
+	private static void escolherRegiao(Scanner sc) {
+		System.out.println("Escolha uma Região:");
+		for (Regiao regiao : Regiao.values()) {
+			System.out.print(regiao.name() + " ");
+		}
+		System.out.println();
+
+		String regiaoEscolhida = sc.nextLine().toUpperCase();
+
+		Regiao regiao = obterRegiaoPorSigla(regiaoEscolhida);
+		if (regiao != null) {
+			System.out.println("Estados da região " + regiao.name() + ":");
+			UF.escreverEstadosPorRegiao(regiao);
+		} else {
+			System.out.println("Região inválida.");
+		}
+	}
+
+	// Método para obter a Regiao a partir da sigla
+	private static Regiao obterRegiaoPorSigla(String sigla) {
+		for (Regiao regiao : Regiao.values()) {
+			if (regiao.name().equals(sigla)) {
+				return regiao;
+			}
+		}
+		return null;
 	}
 
 	static void limpa() {
-        try {
-            if (System.getProperty("os.name").contains("Windows")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else if (System.getProperty("os.name").contains("Linux")
-                    || System.getProperty("os.name").contains("Mac")) {
-                new ProcessBuilder("clear").inheritIO().start().waitFor();
-            } else {
-                System.out.println("Este comando não funciona no seu sistema operacional.");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		try {
+			if (System.getProperty("os.name").contains("Windows")) {
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			} else if (System.getProperty("os.name").contains("Linux")
+					|| System.getProperty("os.name").contains("Mac")) {
+				new ProcessBuilder("clear").inheritIO().start().waitFor();
+			} else {
+				System.out.println("Este comando não funciona no seu sistema operacional.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
